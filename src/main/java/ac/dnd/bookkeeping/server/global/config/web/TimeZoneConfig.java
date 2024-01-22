09667b04d495375d0bd233dbd8b5d146da35238a
sjiwon <sjiwon4491@gmail.com>
2024-01-22 22:53:14 +0900

    chore: add TimeZoneConfig

---
+++ src/main/java/ac/dnd/bookkeeping/server/global/config/web/TimeZoneConfig.java
@@ -0,0 +1,14 @@
+package ac.dnd.bookkeeping.server.global.config.web;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class TimeZoneConfig {
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+}
