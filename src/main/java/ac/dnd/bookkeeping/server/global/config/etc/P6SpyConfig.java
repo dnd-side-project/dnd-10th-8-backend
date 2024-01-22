3f8bcc9757c2437adcb8d23510fa4741ac845cf7
sjiwon <sjiwon4491@gmail.com>
2024-01-22 22:56:38 +0900

    chore: p6spy, Swagger 관련 설정 클래스

---
+++ src/main/java/ac/dnd/bookkeeping/server/global/config/etc/P6SpyConfig.java
@@ -0,0 +1,13 @@
+package ac.dnd.bookkeeping.server.global.config.etc;
+
+import com.p6spy.engine.spy.P6SpyOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class P6SpyConfig {
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6SpyFormatter.class.getName());
+    }
+}
