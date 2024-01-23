dd46cb97b54b120009acbcdf5230d20442581e93
sjiwon <sjiwon4491@gmail.com>
2024-01-23 12:57:49 +0900

    feat: RDB DatabaseCleaner를 활용한 Extension Callback

---
+++ src/test/java/ac/dnd/bookkeeping/server/common/containers/callback/DatabaseCleanerAllCallbackExtension.java
@@ -0,0 +1,25 @@
+package ac.dnd.bookkeeping.server.common.containers.callback;
+
+import ac.dnd.bookkeeping.server.common.utils.DatabaseCleaner;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.platform.commons.support.ModifierSupport;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+public class DatabaseCleanerAllCallbackExtension implements AfterAllCallback {
+    @Override
+    public void afterAll(final ExtensionContext context) {
+        if (context.getTestClass().isPresent()) {
+            final Class<?> currentClass = context.getTestClass().get();
+            if (isNestedClass(currentClass)) {
+                return;
+            }
+        }
+        final DatabaseCleaner databaseCleaner = SpringExtension.getApplicationContext(context).getBean(DatabaseCleaner.class);
+        databaseCleaner.cleanUpDatabase();
+    }
+
+    private boolean isNestedClass(final Class<?> currentClass) {
+        return !ModifierSupport.isStatic(currentClass) && currentClass.isMemberClass();
+    }
+}
