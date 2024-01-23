50f2641380f7de8115a611de28d3f6e568d73255
sjiwon <sjiwon4491@gmail.com>
2024-01-23 12:04:28 +0900

    feat: Mdc, RequestResponseCaching, RequestLogging 필터 관련 FilterRegistrationBean 등록

---
+++ src/main/java/ac/dnd/bookkeeping/server/global/config/web/WebLogConfig.java
@@ -0,0 +1,49 @@
+package ac.dnd.bookkeeping.server.global.config.web;
+
+import ac.dnd.bookkeeping.server.global.filter.MdcLoggingFilter;
+import ac.dnd.bookkeeping.server.global.filter.RequestLoggingFilter;
+import ac.dnd.bookkeeping.server.global.filter.RequestResponseCachingFilter;
+import ac.dnd.bookkeeping.server.global.log.LoggingStatusManager;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebLogConfig {
+    @Bean
+    public FilterRegistrationBean<MdcLoggingFilter> firstFilter() {
+        final FilterRegistrationBean<MdcLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new MdcLoggingFilter());
+        registrationBean.addUrlPatterns("/api/*");
+        registrationBean.setOrder(1);
+        registrationBean.setName("mdcLoggingFilter");
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<RequestResponseCachingFilter> secondFilter() {
+        final FilterRegistrationBean<RequestResponseCachingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestResponseCachingFilter());
+        registrationBean.addUrlPatterns("/api/*");
+        registrationBean.setOrder(2);
+        registrationBean.setName("requestResponseCachingFilter");
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<RequestLoggingFilter> thirdFilter(final LoggingStatusManager loggingStatusManager) {
+        final FilterRegistrationBean<RequestLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestLoggingFilter(
+                loggingStatusManager,
+                "/favicon.ico",
+                "/error*",
+                "/api/swagger*",
+                "/api-docs*",
+                "/actuator*"
+        ));
+        registrationBean.addUrlPatterns("/api/*");
+        registrationBean.setOrder(3);
+        registrationBean.setName("requestLoggingFilter");
+        return registrationBean;
+    }
+}
