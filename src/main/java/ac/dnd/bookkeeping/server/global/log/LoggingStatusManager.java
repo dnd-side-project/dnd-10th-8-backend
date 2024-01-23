5cbb68534e5ef21c7fa389b60ff5e7dce1d627d1
sjiwon <sjiwon4491@gmail.com>
2024-01-23 12:17:18 +0900

    [Feature] MDC, Logback 기반 로깅 메커니즘을 구현한다 (#7)
    * feat: MdcKey, Mdc 기반 추적 필터
    
    * feat: Request/Response 데이터 캐싱 필터
    
    * feat: ThreadLocal 기반 로깅 상태 추적
    
    * feat: Layer별 메소드에 대한 로그 추적기
    
    * feat: Request/Response 데이터 로깅 필터
    
    * feat: Mdc, RequestResponseCaching, RequestLogging 필터 관련 FilterRegistrationBean 등록
    
    * feat: 비동기 처리 관련 Mdc 기반 ThreadPool TaskDecorator
    
    * feat: 응답에 대한 ResponseWrapper
    
    - String, List, ..와 같은 데이터 응답 시 ResponseWrapper 활용해서 객체화
    
    * feat: Layer별 AOP 로깅 메커니즘
    
    * feat: Logback 로깅 설정
    
    * chore: update .gitignore

---
+++ src/main/java/ac/dnd/bookkeeping/server/global/log/LoggingStatusManager.java
@@ -0,0 +1,39 @@
+package ac.dnd.bookkeeping.server.global.log;
+
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+import static ac.dnd.bookkeeping.server.global.log.MdcKey.REQUEST_ID;
+
+@Component
+public class LoggingStatusManager {
+    private final ThreadLocal<LoggingStatus> statusContainer = new ThreadLocal<>();
+
+    public void syncStatus() {
+        final LoggingStatus status = statusContainer.get();
+        if (status == null) {
+            final LoggingStatus firstLoggingStatus = new LoggingStatus(MDC.get(REQUEST_ID.name()));
+            statusContainer.set(firstLoggingStatus);
+        }
+    }
+
+    public LoggingStatus getExistLoggingStatus() {
+        final LoggingStatus status = statusContainer.get();
+        if (status == null) {
+            throw new IllegalStateException("ThreadLocal LoggingStatus not exists...");
+        }
+        return status;
+    }
+
+    public void increaseDepth() {
+        statusContainer.get().increaseDepth();
+    }
+
+    public void decreaseDepth() {
+        statusContainer.get().decreaseDepth();
+    }
+
+    public void clearResource() {
+        statusContainer.remove();
+    }
+}
