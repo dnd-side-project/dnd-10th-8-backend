bc46a8401cdaf9dffede5e2ab9a7d7a488ae948a
sjiwon <sjiwon4491@gmail.com>
2024-01-25 16:08:30 +0900

    feat: 로그아웃 Command/UseCase 구현

---
+++ src/main/java/ac/dnd/bookkeeping/server/auth/application/usecase/LogoutUseCase.java
@@ -0,0 +1,20 @@
+package ac.dnd.bookkeeping.server.auth.application.usecase;
+
+import ac.dnd.bookkeeping.server.auth.application.usecase.command.LogoutCommand;
+import ac.dnd.bookkeeping.server.auth.domain.service.TokenIssuer;
+import ac.dnd.bookkeeping.server.global.annotation.UseCase;
+import ac.dnd.bookkeeping.server.member.domain.model.Member;
+import ac.dnd.bookkeeping.server.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@UseCase
+@RequiredArgsConstructor
+public class LogoutUseCase {
+    private final MemberRepository memberRepository;
+    private final TokenIssuer tokenIssuer;
+
+    public void invoke(final LogoutCommand command) {
+        final Member member = memberRepository.getById(command.memberId());
+        tokenIssuer.deleteRefreshToken(member.getId());
+    }
+}
