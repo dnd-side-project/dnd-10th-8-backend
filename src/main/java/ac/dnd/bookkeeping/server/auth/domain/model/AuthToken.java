c5f66315c342688fee352d0f9ff7d8088668bcb9
sjiwon <sjiwon4491@gmail.com>
2024-01-25 12:51:55 +0900

    feat: 토큰 Incoming 관련 헤더 네이밍 정의

--- src/main/java/ac/dnd/bookkeeping/server/auth/domain/model/AuthToken.java
+++ src/main/java/ac/dnd/bookkeeping/server/auth/domain/model/AuthToken.java
@@ -4,4 +4,6 @@ public record AuthToken(
         String accessToken,
         String refreshToken
 ) {
+    public static final String ACCESS_TOKEN_HEADER = "Authorization";
+    public static final String REFRESH_TOKEN_HEADER = "Token-Refresh";
 }
