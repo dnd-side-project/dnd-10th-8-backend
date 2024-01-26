0466384b076efe18e4befa2993d64d7c6d995a4c
sjiwon <sjiwon4491@gmail.com>
2024-01-26 16:39:58 +0900

    refactor: Member-Gender 분리, 탈퇴 시 nullable 데이터 수정

--- src/main/java/ac/dnd/bookkeeping/server/member/domain/model/Member.java
+++ src/main/java/ac/dnd/bookkeeping/server/member/domain/model/Member.java
@@ -1,7 +1,6 @@
 package ac.dnd.bookkeeping.server.member.domain.model;
 
 import ac.dnd.bookkeeping.server.global.base.BaseEntity;
-import ac.dnd.bookkeeping.server.member.exception.MemberException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -9,15 +8,12 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 
 import static ac.dnd.bookkeeping.server.member.domain.model.Member.Status.ACTIVE;
 import static ac.dnd.bookkeeping.server.member.domain.model.Member.Status.INACTIVE;
-import static ac.dnd.bookkeeping.server.member.exception.MemberExceptionCode.INVALID_GENDER;
 import static jakarta.persistence.EnumType.STRING;
 import static lombok.AccessLevel.PROTECTED;
 
@@ -63,44 +59,24 @@ public class Member extends BaseEntity<Member> {
         this.status = status;
     }
 
-    public static Member create(final SocialPlatform platform, final String profileImageUrl) {
-        return new Member(platform, profileImageUrl, null, null, null, ACTIVE);
+    public static Member create(
+            final SocialPlatform platform,
+            final String profileImageUrl,
+            final Nickname nickname,
+            final Gender gender,
+            final LocalDate birth
+    ) {
+        return new Member(platform, profileImageUrl, nickname, gender, birth, ACTIVE);
     }
 
     public void syncEmail(final Email email) {
         this.platform = platform.syncEmail(email);
     }
 
-    public void complete(final Nickname nickname, final Gender gender, final LocalDate birth) {
-        this.nickname = nickname;
-        this.gender = gender;
-        this.birth = birth;
-    }
-
     public void delete() {
-        this.platform = null;
-        this.profileImageUrl = null;
-        this.nickname = null;
-        this.gender = null;
-        this.birth = null;
         this.status = INACTIVE;
-    }
-
-    @Getter
-    @RequiredArgsConstructor
-    public enum Gender {
-        MAIL("male"),
-        FEMAIL("female"),
-        ;
-
-        private final String value;
-
-        public static Gender from(final String value) {
-            return Arrays.stream(values())
-                    .filter(it -> it.value.equals(value))
-                    .findFirst()
-                    .orElseThrow(() -> new MemberException(INVALID_GENDER));
-        }
+        this.platform = null;
+        this.nickname = null;
     }
 
     public enum Status {
--- src/test/java/ac/dnd/bookkeeping/server/member/domain/model/MemberTest.java
+++ src/test/java/ac/dnd/bookkeeping/server/member/domain/model/MemberTest.java
@@ -26,34 +26,6 @@ class MemberTest extends UnitTest {
     }
 
     @Test
-    @DisplayName("온보딩 과정에서 추가 정보들을 기입한다 (닉네임, 성별, 생년월일)")
-    void complete() {
-        // given
-        final Member member = Member.create(MEMBER_1.getPlatform(), MEMBER_1.getProfileImageUrl());
-        assertAll(
-                () -> assertThat(member.getPlatform().getSocialId()).isEqualTo(MEMBER_1.getPlatform().getSocialId()),
-                () -> assertThat(member.getPlatform().getEmail().getValue()).isEqualTo(MEMBER_1.getPlatform().getEmail().getValue()),
-                () -> assertThat(member.getProfileImageUrl()).isEqualTo(MEMBER_1.getProfileImageUrl()),
-                () -> assertThat(member.getNickname()).isNull(),
-                () -> assertThat(member.getGender()).isNull(),
-                () -> assertThat(member.getBirth()).isNull()
-        );
-
-        // when
-        member.complete(MEMBER_2.getNickname(), MEMBER_1.getGender(), MEMBER_1.getBirth());
-
-        // then
-        assertAll(
-                () -> assertThat(member.getPlatform().getSocialId()).isEqualTo(MEMBER_1.getPlatform().getSocialId()),
-                () -> assertThat(member.getPlatform().getEmail().getValue()).isEqualTo(MEMBER_1.getPlatform().getEmail().getValue()),
-                () -> assertThat(member.getProfileImageUrl()).isEqualTo(MEMBER_1.getProfileImageUrl()),
-                () -> assertThat(member.getNickname().getValue()).isEqualTo(MEMBER_2.getNickname().getValue()),
-                () -> assertThat(member.getGender()).isEqualTo(MEMBER_1.getGender()),
-                () -> assertThat(member.getBirth()).isEqualTo(MEMBER_1.getBirth())
-        );
-    }
-
-    @Test
     @DisplayName("탈퇴 처리를 진행한다")
     void delete() {
         // given
@@ -66,10 +38,10 @@ class MemberTest extends UnitTest {
         assertAll(
                 () -> assertThat(member.getId()).isEqualTo(1L),
                 () -> assertThat(member.getPlatform()).isNull(),
-                () -> assertThat(member.getProfileImageUrl()).isNull(),
+                () -> assertThat(member.getProfileImageUrl()).isEqualTo(MEMBER_1.getProfileImageUrl()),
                 () -> assertThat(member.getNickname()).isNull(),
-                () -> assertThat(member.getGender()).isNull(),
-                () -> assertThat(member.getBirth()).isNull(),
+                () -> assertThat(member.getGender()).isEqualTo(MEMBER_1.getGender()),
+                () -> assertThat(member.getBirth()).isEqualTo(MEMBER_1.getBirth()),
                 () -> assertThat(member.getStatus()).isEqualTo(INACTIVE)
         );
     }
