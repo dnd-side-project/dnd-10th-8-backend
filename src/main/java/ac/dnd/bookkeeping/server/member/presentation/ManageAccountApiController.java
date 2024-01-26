207b300a27b609a2a6e9fc8906f2eeb560596e02
sjiwon <sjiwon4491@gmail.com>
2024-01-26 16:40:40 +0900

    remove: 온보딩 추가 정보 기입 관련 API, 로직 제거

--- src/main/java/ac/dnd/bookkeeping/server/member/presentation/ManageAccountApiController.java
+++ src/main/java/ac/dnd/bookkeeping/server/member/presentation/ManageAccountApiController.java
@@ -4,10 +4,8 @@ import ac.dnd.bookkeeping.server.auth.domain.model.Authenticated;
 import ac.dnd.bookkeeping.server.global.annotation.Auth;
 import ac.dnd.bookkeeping.server.global.dto.ResponseWrapper;
 import ac.dnd.bookkeeping.server.member.application.usecase.DeleteAccountUseCase;
-import ac.dnd.bookkeeping.server.member.application.usecase.ManageResourceUseCase;
-import ac.dnd.bookkeeping.server.member.application.usecase.command.CompleteInfoCommand;
+import ac.dnd.bookkeeping.server.member.application.usecase.ManageAccountUseCase;
 import ac.dnd.bookkeeping.server.member.presentation.dto.request.CheckNicknameRequest;
-import ac.dnd.bookkeeping.server.member.presentation.dto.request.CompleteInfoRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -16,8 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class ManageAccountApiController {
-    private final ManageResourceUseCase manageResourceUseCase;
+    private final ManageAccountUseCase manageAccountUseCase;
     private final DeleteAccountUseCase deleteAccountUseCase;
 
     @Operation(summary = "닉네임 중복 체크 Endpoint")
@@ -34,23 +30,8 @@ public class ManageAccountApiController {
     public ResponseEntity<ResponseWrapper<Boolean>> checkNickname(
             @ModelAttribute @Valid final CheckNicknameRequest request
     ) {
-        final boolean result = manageResourceUseCase.isUniqueNickname(request.toNickname());
+        final boolean result = manageAccountUseCase.isUniqueNickname(request.toNickname());
         return ResponseEntity.ok(ResponseWrapper.from(result));
-    }
-
-    @Operation(summary = "온보딩 사용자 추가 정보 기입 Endpoint")
-    @PatchMapping("/v1/members/me/complete")
-    public ResponseEntity<Void> complete(
-            @Auth final Authenticated authenticated,
-            @RequestBody @Valid final CompleteInfoRequest request
-    ) {
-        manageResourceUseCase.completeInfo(new CompleteInfoCommand(
-                authenticated.id(),
-                request.toNickname(),
-                request.toGender(),
-                request.birth()
-        ));
-        return ResponseEntity.noContent().build();
     }
 
     @Operation(summary = "사용자 탙퇴 처리 Endpoint")
--- src/test/java/ac/dnd/bookkeeping/server/member/presentation/ManageAccountApiControllerTest.java
+++ src/test/java/ac/dnd/bookkeeping/server/member/presentation/ManageAccountApiControllerTest.java
@@ -1,9 +1,8 @@
 package ac.dnd.bookkeeping.server.member.presentation;
 
 import ac.dnd.bookkeeping.server.common.ControllerTest;
-import ac.dnd.bookkeeping.server.member.application.usecase.ManageResourceUseCase;
+import ac.dnd.bookkeeping.server.member.application.usecase.ManageAccountUseCase;
 import ac.dnd.bookkeeping.server.member.domain.model.Member;
-import ac.dnd.bookkeeping.server.member.presentation.dto.request.CompleteInfoRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -19,7 +18,6 @@ import static ac.dnd.bookkeeping.server.common.utils.RestDocsSpecificationUtils.
 import static ac.dnd.bookkeeping.server.common.utils.RestDocsSpecificationUtils.successDocsWithAccessToken;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("Member -> ManageAccountApiController 테스트")
 class ManageAccountApiControllerTest extends ControllerTest {
     @Autowired
-    private ManageResourceUseCase manageResourceUseCase;
+    private ManageAccountUseCase manageAccountUseCase;
 
     @Nested
     @DisplayName("닉네임 중복 체크 API [GET /api/v1/check-nickname]")
@@ -38,51 +36,18 @@ class ManageAccountApiControllerTest extends ControllerTest {
         @DisplayName("닉네임 사용 가능 여부를 조회한다")
         void success() {
             // given
-            given(manageResourceUseCase.isUniqueNickname(any())).willReturn(true);
+            given(manageAccountUseCase.isUniqueNickname(any())).willReturn(true);
 
             // when - then
             successfulExecute(
                     getRequest(BASE_URL, Map.of("nickname", "Hello")),
                     status().isOk(),
-                    successDocs("MemberApi/Onboarding/CheckNickname", createHttpSpecSnippets(
+                    successDocs("MemberApi/Register/CheckNickname", createHttpSpecSnippets(
                             queryParameters(
                                     query("nickname", "중복 체크할 닉네임", true)
                             ),
                             responseFields(
                                     body("result", "닉네임 사용 가능 여부")
-                            )
-                    ))
-            );
-        }
-    }
-
-    @Nested
-    @DisplayName("사용자 추가 정보 기입 API [POST /api/v1/members/me/complete]")
-    class CompleteInfo {
-        private static final String BASE_URL = "/api/v1/members/me/complete";
-
-        private final CompleteInfoRequest request = new CompleteInfoRequest(
-                MEMBER_1.getNickname().getValue(),
-                MEMBER_1.getGender().getValue(),
-                MEMBER_1.getBirth()
-        );
-        private final Member member = MEMBER_1.toDomain().apply(1L);
-
-        @Test
-        @DisplayName("온보딩 후 추가 정보를 기입한다")
-        void success() {
-            // given
-            applyToken(true, member.getId());
-
-            // when - then
-            successfulExecute(
-                    patchRequestWithAccessToken(BASE_URL, request),
-                    status().isNoContent(),
-                    successDocsWithAccessToken("MemberApi/Onboarding/CompleteInfo", createHttpSpecSnippets(
-                            requestFields(
-                                    body("nickname", "닉네임", true),
-                                    body("gender", "성별", "male / female", true),
-                                    body("birth", "생년월일", "LocalDate 형식 (yyyy-MM-dd)", true)
                             )
                     ))
             );
