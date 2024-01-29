6eac326682c651cc006dabc34debaf6142ac0673
sjiwon <sjiwon4491@gmail.com>
2024-01-30 00:37:30 +0900

    feat: S3 버킷 파일이름 생성기

---
+++ src/main/java/ac/dnd/mur/server/file/domain/service/DefaultBucketFileNameGenerator.java
@@ -0,0 +1,15 @@
+package ac.dnd.mur.server.file.domain.service;
+
+import ac.dnd.mur.server.file.domain.model.FileExtension;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class DefaultBucketFileNameGenerator implements BucketFileNameGenerator {
+    @Override
+    public String get(final String fileName) {
+        final FileExtension extension = FileExtension.getExtensionViaFimeName(fileName);
+        return UUID.randomUUID() + extension.getValue();
+    }
+}
