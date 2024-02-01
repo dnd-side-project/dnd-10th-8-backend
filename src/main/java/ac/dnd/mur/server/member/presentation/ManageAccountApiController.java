fadce497dfa92279914d6ea4e82902e36c4fba82
sjiwon <sjiwon4491@gmail.com>
2024-02-01 19:12:40 +0900

    refactor: 회원가입 플로우, 사용자 계정 관리 API Controller 분리

--- src/main/java/ac/dnd/mur/server/member/presentation/ManageAccountApiController.java
+++ src/main/java/ac/dnd/mur/server/member/presentation/ManageAccountApiController.java
@@ -1,17 +1,10 @@
 package ac.dnd.mur.server.member.presentation;
 
-import ac.dnd.mur.server.auth.domain.model.AuthMember;
 import ac.dnd.mur.server.auth.domain.model.Authenticated;
 import ac.dnd.mur.server.global.annotation.Auth;
-import ac.dnd.mur.server.global.dto.ResponseWrapper;
 import ac.dnd.mur.server.member.application.usecase.DeleteAccountUseCase;
-import ac.dnd.mur.server.member.application.usecase.ManageResourceUseCase;
-import ac.dnd.mur.server.member.application.usecase.RegisterAccountUseCase;
 import ac.dnd.mur.server.member.application.usecase.UpdateMemberUseCase;
-import ac.dnd.mur.server.member.application.usecase.command.RegisterMemberCommand;
 import ac.dnd.mur.server.member.application.usecase.command.UpdateMemberCommand;
-import ac.dnd.mur.server.member.presentation.dto.request.CheckNicknameRequest;
-import ac.dnd.mur.server.member.presentation.dto.request.RegisterMemberRequest;
 import ac.dnd.mur.server.member.presentation.dto.request.UpdateMemberRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,10 +12,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,35 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class ManageAccountApiController {
-    private final ManageResourceUseCase manageResourceUseCase;
-    private final RegisterAccountUseCase registerAccountUseCase;
     private final UpdateMemberUseCase updateMemberUseCase;
     private final DeleteAccountUseCase deleteAccountUseCase;
-
-    @Operation(summary = "닉네임 중복 체크 Endpoint")
-    @GetMapping("/v1/members/check-nickname")
-    public ResponseEntity<ResponseWrapper<Boolean>> checkNickname(
-            @ModelAttribute @Valid final CheckNicknameRequest request
-    ) {
-        final boolean result = manageResourceUseCase.isUniqueNickname(request.toNickname());
-        return ResponseEntity.ok(ResponseWrapper.from(result));
-    }
-
-    @Operation(summary = "회원가입 + 로그인 처리 Endpoint")
-    @PostMapping("/v1/members")
-    public ResponseEntity<AuthMember> register(
-            @RequestBody @Valid final RegisterMemberRequest request
-    ) {
-        final AuthMember response = registerAccountUseCase.invoke(new RegisterMemberCommand(
-                request.toSocialPlatform(),
-                request.profileImageUrl(),
-                request.name(),
-                request.toNickname(),
-                request.toGender(),
-                request.birth()
-        ));
-        return ResponseEntity.ok(response);
-    }
 
     @Operation(summary = "마이페이지 정보 수정 Endpoint")
     @PatchMapping("/v1/members/me")
--- src/test/java/ac/dnd/mur/server/acceptance/member/ManageAccountAcceptanceTest.java
+++ src/test/java/ac/dnd/mur/server/acceptance/member/ManageAccountAcceptanceTest.java
@@ -11,52 +11,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import static ac.dnd.mur.server.acceptance.member.MemberAcceptanceStep.내_정보를_수정한다;
 import static ac.dnd.mur.server.acceptance.member.MemberAcceptanceStep.닉네임_중복_체크를_진행한다;
 import static ac.dnd.mur.server.acceptance.member.MemberAcceptanceStep.서비스_탈퇴를_진행한다;
-import static ac.dnd.mur.server.acceptance.member.MemberAcceptanceStep.회원가입을_진행한다;
 import static ac.dnd.mur.server.common.fixture.MemberFixture.MEMBER_1;
 import static ac.dnd.mur.server.common.fixture.MemberFixture.MEMBER_2;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
 @ExtendWith(DatabaseCleanerEachCallbackExtension.class)
 @DisplayName("[Acceptance Test] 사용자 계정 관리 관련 기능")
 public class ManageAccountAcceptanceTest extends AcceptanceTest {
-    @Nested
-    @DisplayName("닉네임 중복 체크 API")
-    class CheckNickname {
-        @Test
-        @DisplayName("사용할 수 있는 닉네임인지 확인한다 [True]")
-        void resultTrue() {
-            닉네임_중복_체크를_진행한다("test")
-                    .statusCode(OK.value())
-                    .body("result", is(true));
-        }
-
-        @Test
-        @DisplayName("사용할 수 있는 닉네임인지 확인한다 [False]")
-        void resultFalse() {
-            MEMBER_1.회원가입과_로그인을_진행한다();
-            닉네임_중복_체크를_진행한다(MEMBER_1.getNickname().getValue())
-                    .statusCode(OK.value())
-                    .body("result", is(false));
-        }
-    }
-
-    @Nested
-    @DisplayName("회원가입 API")
-    class Register {
-        @Test
-        @DisplayName("회원가입을 진행한다")
-        void success() {
-            회원가입을_진행한다(MEMBER_1)
-                    .statusCode(OK.value())
-                    .body("id", notNullValue(Long.class))
-                    .body("accessToken", notNullValue(String.class))
-                    .body("refreshToken", notNullValue(String.class));
-        }
-    }
-
     @Nested
     @DisplayName("내 정보 수정 API")
     class Update {
