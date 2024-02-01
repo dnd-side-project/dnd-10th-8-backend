d8405edd029c4e63e2d05e361c59b5f8962113f0
sjiwon <sjiwon4491@gmail.com>
2024-02-01 20:41:28 +0900

    refactor: add tx scope

--- src/main/java/ac/dnd/mur/server/group/application/usecase/AddGroupUseCase.java
+++ src/main/java/ac/dnd/mur/server/group/application/usecase/AddGroupUseCase.java
@@ -1,5 +1,6 @@
 package ac.dnd.mur.server.group.application.usecase;
 
+import ac.dnd.mur.server.global.annotation.MurWritableTransactional;
 import ac.dnd.mur.server.global.annotation.UseCase;
 import ac.dnd.mur.server.group.application.usecase.command.AddGroupCommand;
 import ac.dnd.mur.server.group.domain.model.Group;
@@ -17,6 +18,7 @@ public class AddGroupUseCase {
     private final MemberRepository memberRepository;
     private final GroupRepository groupRepository;
 
+    @MurWritableTransactional
     public long invoke(final AddGroupCommand command) {
         final Member member = memberRepository.getById(command.memberId());
         validateExistsGroup(member, command.name());
