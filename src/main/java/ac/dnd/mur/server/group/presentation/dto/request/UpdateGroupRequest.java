90589efc08208dce25c5b6f61758b9b1399ec070
sjiwon <sjiwon4491@gmail.com>
2024-02-02 19:24:56 +0900

    feat: 그룹명 수정 Endpoint, 인수 테스트 작성

---
+++ src/main/java/ac/dnd/mur/server/group/presentation/dto/request/UpdateGroupRequest.java
@@ -0,0 +1,9 @@
+package ac.dnd.mur.server.group.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record UpdateGroupRequest(
+        @NotBlank(message = "그룹 이름은 필수입니다.")
+        String name
+) {
+}
