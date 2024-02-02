77f6c1ba813a8e189b8177c53e155f271d2bbe75
sjiwon <sjiwon4491@gmail.com>
2024-02-02 22:11:49 +0900

    feat: 마음 도메인 생명주기(생성, 수정, 삭제) 관련 Endpoint, 인수 테스트 작성

---
+++ src/test/java/ac/dnd/mur/server/acceptance/heart/HeartAcceptanceStep.java
@@ -0,0 +1,83 @@
+package ac.dnd.mur.server.acceptance.heart;
+
+import ac.dnd.mur.server.heart.presentation.dto.request.CreateHeartRequest;
+import ac.dnd.mur.server.heart.presentation.dto.request.UpdateHeartRequest;
+import io.restassured.response.ValidatableResponse;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static ac.dnd.mur.server.acceptance.CommonRequestFixture.deleteRequestWithAccessToken;
+import static ac.dnd.mur.server.acceptance.CommonRequestFixture.patchRequestWithAccessToken;
+import static ac.dnd.mur.server.acceptance.CommonRequestFixture.postRequestWithAccessToken;
+
+public class HeartAcceptanceStep {
+    public static ValidatableResponse 마음을_생성한다(
+            final long relationId,
+            final boolean give,
+            final long money,
+            final LocalDate day,
+            final String event,
+            final String memo,
+            final List<String> tags,
+            final String accessToken
+    ) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/v1/hearts")
+                .build()
+                .toUri()
+                .getPath();
+
+        final CreateHeartRequest request = new CreateHeartRequest(relationId, give, money, day, event, memo, tags);
+
+        return postRequestWithAccessToken(uri, request, accessToken);
+    }
+
+    public static long 마음을_생성하고_ID를_추출한다(
+            final long relationId,
+            final boolean give,
+            final long money,
+            final LocalDate day,
+            final String event,
+            final String memo,
+            final List<String> tags,
+            final String accessToken
+    ) {
+        return 마음을_생성한다(relationId, give, money, day, event, memo, tags, accessToken)
+                .extract()
+                .jsonPath()
+                .getLong("result");
+    }
+
+    public static ValidatableResponse 마음을_수정한다(
+            final long heartId,
+            final long money,
+            final LocalDate day,
+            final String event,
+            final String memo,
+            final List<String> tags,
+            final String accessToken
+    ) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/v1/hearts/{heartId}")
+                .build(heartId)
+                .getPath();
+
+        final UpdateHeartRequest request = new UpdateHeartRequest(money, day, event, memo, tags);
+
+        return patchRequestWithAccessToken(uri, request, accessToken);
+    }
+
+    public static ValidatableResponse 마음을_삭제한다(
+            final long heartId,
+            final String accessToken
+    ) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/v1/hearts/{heartId}")
+                .build(heartId)
+                .getPath();
+
+        return deleteRequestWithAccessToken(uri, accessToken);
+    }
+}
