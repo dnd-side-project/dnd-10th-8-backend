0eaa2a3567bb77a53fc205d4924be91f9b03de74
sjiwon <sjiwon4491@gmail.com>
2024-02-02 13:30:31 +0900

    feat: 관계 생성 Command/UseCase

---
+++ src/main/java/ac/dnd/mur/server/relation/application/CreateRelationUseCase.java
@@ -0,0 +1,34 @@
+package ac.dnd.mur.server.relation.application;
+
+import ac.dnd.mur.server.global.annotation.MurWritableTransactional;
+import ac.dnd.mur.server.global.annotation.UseCase;
+import ac.dnd.mur.server.group.domain.model.Group;
+import ac.dnd.mur.server.group.domain.repository.GroupRepository;
+import ac.dnd.mur.server.member.domain.model.Member;
+import ac.dnd.mur.server.member.domain.repository.MemberRepository;
+import ac.dnd.mur.server.relation.application.usecase.command.CreateRelationCommand;
+import ac.dnd.mur.server.relation.domain.model.Relation;
+import ac.dnd.mur.server.relation.domain.repository.RelationRepository;
+import lombok.RequiredArgsConstructor;
+
+@UseCase
+@RequiredArgsConstructor
+public class CreateRelationUseCase {
+    private final MemberRepository memberRepository;
+    private final GroupRepository groupRepository;
+    private final RelationRepository relationRepository;
+
+    @MurWritableTransactional
+    public long invoke(final CreateRelationCommand command) {
+        final Member member = memberRepository.getById(command.memberId());
+        final Group group = groupRepository.getMemberGroup(command.groupId(), command.memberId());
+
+        return relationRepository.save(new Relation(
+                member,
+                group,
+                command.name(),
+                command.phone(),
+                command.memo()
+        )).getId();
+    }
+}
