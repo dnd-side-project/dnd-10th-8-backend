8cf49dc0d6d3b770979b2c17e4b758be8292a467
sjiwon <sjiwon4491@gmail.com>
2024-02-05 19:22:48 +0900

    feat: 월별, 연도별 자신의 주고받은 마음 통계 쿼리

---
+++ src/main/java/ac/dnd/mur/server/heart/domain/repository/query/HeartStatisticsRepositoryImpl.java
@@ -0,0 +1,62 @@
+package ac.dnd.mur.server.heart.domain.repository.query;
+
+import ac.dnd.mur.server.global.annotation.MurReadOnlyTransactional;
+import ac.dnd.mur.server.heart.domain.repository.query.response.PersonalHeartHistory;
+import ac.dnd.mur.server.heart.domain.repository.query.response.QPersonalHeartHistory;
+import ac.dnd.mur.server.heart.domain.repository.query.spec.PersonalStatisticsCondition;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static ac.dnd.mur.server.group.domain.model.QGroup.group;
+import static ac.dnd.mur.server.heart.domain.model.QHeart.heart;
+import static ac.dnd.mur.server.relation.domain.model.QRelation.relation;
+
+@Repository
+@MurReadOnlyTransactional
+@RequiredArgsConstructor
+public class HeartStatisticsRepositoryImpl implements HeartStatisticsRepository {
+    private final JPAQueryFactory query;
+
+    @Override
+    public List<PersonalHeartHistory> fetchPersonalHeartHistories(final PersonalStatisticsCondition condition) {
+        return query
+                .select(new QPersonalHeartHistory(
+                        heart.event,
+                        relation.name,
+                        group.name,
+                        heart.money,
+                        heart.day,
+                        heart.memo
+                ))
+                .from(heart)
+                .innerJoin(relation).on(relation.id.eq(heart.relationId))
+                .innerJoin(group).on(group.id.eq(relation.groupId))
+                .where(
+                        isGive(condition.give()),
+                        byYearOrMonth(condition.type(), condition.year(), condition.month())
+                )
+                .orderBy(heart.day.desc(), heart.id.desc())
+                .fetch();
+    }
+
+    private BooleanExpression isGive(final boolean give) {
+        return give ? heart.give.isTrue() : heart.give.isFalse();
+    }
+
+    private BooleanExpression byYearOrMonth(final PersonalStatisticsCondition.Type type, final int year, final int month) {
+        if (type == PersonalStatisticsCondition.Type.YEAR) {
+            final LocalDate start = LocalDate.of(year, 1, 1);
+            final LocalDate end = start.plusYears(1);
+            return heart.day.goe(start).and(heart.day.lt(end));
+        }
+
+        final LocalDate start = LocalDate.of(year, month, 1);
+        final LocalDate end = start.plusMonths(1);
+        return heart.day.goe(start).and(heart.day.lt(end));
+    }
+}
---
+++ src/main/java/ac/dnd/mur/server/heart/domain/repository/query/spec/PersonalStatisticsCondition.java
@@ -0,0 +1,45 @@
+package ac.dnd.mur.server.heart.domain.repository.query.spec;
+
+import ac.dnd.mur.server.global.exception.GlobalException;
+import ac.dnd.mur.server.heart.exception.HeartException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+import static ac.dnd.mur.server.global.exception.GlobalExceptionCode.VALIDATION_ERROR;
+import static ac.dnd.mur.server.heart.exception.HeartExceptionCode.INVALID_TYPE;
+
+public record PersonalStatisticsCondition(
+        Type type,
+        int year,
+        int month,
+        boolean give
+) {
+    public PersonalStatisticsCondition {
+        if (type == Type.YEAR && (year < 1900 || year > 2100)) {
+            throw new GlobalException(VALIDATION_ERROR);
+        }
+
+        if (type == Type.MONTH && (year < 1900 || year > 2100) && (month < 1 || month > 12)) {
+            throw new GlobalException(VALIDATION_ERROR);
+        }
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum Type {
+        YEAR("year"),
+        MONTH("month"),
+        ;
+
+        private final String value;
+
+        public static Type from(final String value) {
+            return Arrays.stream(values())
+                    .filter(it -> it.value.equals(value))
+                    .findFirst()
+                    .orElseThrow(() -> new HeartException(INVALID_TYPE));
+        }
+    }
+}
