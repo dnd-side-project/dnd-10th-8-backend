1a65cf68dd33bf09059cd6623639401e87888f50
sjiwon <sjiwon4491@gmail.com>
2024-02-07 20:47:08 +0900

    feat: 알람 동기화에 필요한 스케줄 데이터 조회 쿼리

--- src/main/java/ac/dnd/mur/server/schedule/domain/repository/query/ScheduleQueryRepository.java
+++ src/main/java/ac/dnd/mur/server/schedule/domain/repository/query/ScheduleQueryRepository.java
@@ -1,6 +1,7 @@
 package ac.dnd.mur.server.schedule.domain.repository.query;
 
 import ac.dnd.mur.server.schedule.domain.repository.query.response.CalendarSchedule;
+import ac.dnd.mur.server.schedule.domain.repository.query.response.ScheduleForAlarm;
 import ac.dnd.mur.server.schedule.domain.repository.query.response.UnrecordedSchedule;
 import ac.dnd.mur.server.schedule.domain.repository.query.spec.SearchCalendarScheduleCondition;
 
@@ -10,4 +11,6 @@ public interface ScheduleQueryRepository {
     List<UnrecordedSchedule> fetchUnrecordedSchedules(final List<Long> scheduleIds);
 
     List<CalendarSchedule> fetchCalendarSchedules(final SearchCalendarScheduleCondition condition);
+
+    List<ScheduleForAlarm> fetchSchedulesForAlarm(final long memberId);
 }
