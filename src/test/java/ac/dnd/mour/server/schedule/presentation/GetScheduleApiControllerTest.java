a6899338e486426fcdb1edcde5d76b022fee274e
sjiwon <sjiwon4491@gmail.com>
2024-02-13 21:22:49 +0900

    feat: 일정 단건 조회 Endpoint, 인수 테스트 작성

--- src/test/java/ac/dnd/mour/server/schedule/presentation/GetScheduleApiControllerTest.java
+++ src/test/java/ac/dnd/mour/server/schedule/presentation/GetScheduleApiControllerTest.java
@@ -5,9 +5,11 @@ import ac.dnd.mour.server.group.domain.model.GroupResponse;
 import ac.dnd.mour.server.member.domain.model.Member;
 import ac.dnd.mour.server.relation.domain.model.response.RelationSummary;
 import ac.dnd.mour.server.schedule.application.usecase.GetCalendarScheduleUseCase;
+import ac.dnd.mour.server.schedule.application.usecase.GetScheduleDetailsUseCase;
 import ac.dnd.mour.server.schedule.application.usecase.GetSchedulesForAlarmUseCase;
 import ac.dnd.mour.server.schedule.application.usecase.GetUnrecordedScheduleUseCase;
 import ac.dnd.mour.server.schedule.application.usecase.query.response.CalendarScheduleResponse;
+import ac.dnd.mour.server.schedule.application.usecase.query.response.ScheduleDetailsResponse;
 import ac.dnd.mour.server.schedule.application.usecase.query.response.SchedulesForAlarmResponse;
 import ac.dnd.mour.server.schedule.application.usecase.query.response.UnrecordedScheduleResponse;
 import org.junit.jupiter.api.DisplayName;
@@ -24,17 +26,22 @@ import static ac.dnd.mour.server.common.fixture.MemberFixture.MEMBER_1;
 import static ac.dnd.mour.server.common.fixture.RelationFixture.친구_1;
 import static ac.dnd.mour.server.common.fixture.ScheduleFixture.친구_XXX_생일;
 import static ac.dnd.mour.server.common.utils.RestDocsSpecificationUtils.SnippetFactory.body;
+import static ac.dnd.mour.server.common.utils.RestDocsSpecificationUtils.SnippetFactory.path;
 import static ac.dnd.mour.server.common.utils.RestDocsSpecificationUtils.SnippetFactory.query;
 import static ac.dnd.mour.server.common.utils.RestDocsSpecificationUtils.createHttpSpecSnippets;
 import static ac.dnd.mour.server.common.utils.RestDocsSpecificationUtils.successDocsWithAccessToken;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("Schedule -> GetScheduleApiController 테스트")
 class GetScheduleApiControllerTest extends ControllerTest {
+    @Autowired
+    private GetScheduleDetailsUseCase getScheduleDetailsUseCase;
+
     @Autowired
     private GetUnrecordedScheduleUseCase getUnrecordedScheduleUseCase;
 
@@ -45,6 +52,63 @@ class GetScheduleApiControllerTest extends ControllerTest {
     private GetSchedulesForAlarmUseCase getSchedulesForAlarmUseCase;
 
     private final Member member = MEMBER_1.toDomain().apply(1L);
+
+    @Nested
+    @DisplayName("일정 상세 조회 API [GET /api/v1/schedules/me/{scheduleId}]")
+    class GetScheduleDetails {
+        private static final String BASE_URL = "/api/v1/schedules/me/{scheduleId}";
+
+        @Test
+        @DisplayName("일정 상세 정보를 조회한다")
+        void success() {
+            // given
+            applyToken(true, member);
+            given(getScheduleDetailsUseCase.invoke(any())).willReturn(new ScheduleDetailsResponse(
+                    10L,
+                    new RelationSummary(
+                            1L,
+                            친구_1.getName(),
+                            new GroupResponse(1L, 친구.getName())
+                    ),
+                    LocalDate.of(2024, 1, 31),
+                    친구_XXX_생일.getEvent(),
+                    친구_XXX_생일.getRepeat().getType().getValue(),
+                    친구_XXX_생일.getRepeat().getFinish(),
+                    친구_XXX_생일.getAlarm(),
+                    친구_XXX_생일.getTime(),
+                    친구_XXX_생일.getLink(),
+                    친구_XXX_생일.getLocation(),
+                    친구_XXX_생일.getMemo()
+            ));
+
+            // when - then
+            successfulExecute(
+                    getRequestWithAccessToken(new UrlWithVariables(BASE_URL, 1L)),
+                    status().isOk(),
+                    successDocsWithAccessToken("ScheduleApi/Details", createHttpSpecSnippets(
+                            pathParameters(
+                                    path("scheduleId", "일정 ID(PK)", true)
+                            ),
+                            responseFields(
+                                    body("id", "일정 ID(PK)"),
+                                    body("relation.id", "관계 ID(PK)"),
+                                    body("relation.name", "관계 이름"),
+                                    body("relation.group.id", "그룹 ID(PK)"),
+                                    body("relation.group.name", "그룹명"),
+                                    body("day", "날짜"),
+                                    body("event", "행사 종류"),
+                                    body("repeatType", "반복 타입", "Nullable"),
+                                    body("repeatFinish", "반복 종료 날짜", "Nullable"),
+                                    body("alarm", "알람", "Nullable"),
+                                    body("time", "시간", "Nullable"),
+                                    body("link", "링크", "Nullable"),
+                                    body("location", "위치", "Nullable"),
+                                    body("memo", "메모", "Nullable")
+                            )
+                    ))
+            );
+        }
+    }
 
     @Nested
     @DisplayName("지출(보낸 마음)이 기록되지 않은 일정 조회 API [GET /api/v1/schedules/unrecorded]")
