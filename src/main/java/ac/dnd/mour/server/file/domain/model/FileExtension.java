87a95ce7afcf7808a4708edba05b806b218b04cd
sjiwon <sjiwon4491@gmail.com>
2024-02-27 23:35:36 +0900

    refactor: FileExtension `equalsIgnoreCase`

--- src/main/java/ac/dnd/mour/server/file/domain/model/FileExtension.java
+++ src/main/java/ac/dnd/mour/server/file/domain/model/FileExtension.java
@@ -12,21 +12,21 @@ import static ac.dnd.mour.server.file.exception.FileExceptionCode.INVALID_FILE_E
 @RequiredArgsConstructor
 public enum FileExtension {
     // 프로필 사진
-    JPG(".jpg"),
-    JPEG(".jpeg"),
-    PNG(".png"),
+    JPG("jpg"),
+    JPEG("jpeg"),
+    PNG("png"),
     ;
 
     private final String value;
 
     public static FileExtension getExtensionViaFimeName(final String fileName) {
         return Arrays.stream(values())
-                .filter(it -> it.value.equals(extractFileExtension(fileName)))
+                .filter(it -> it.value.equalsIgnoreCase(extractFileExtension(fileName)))
                 .findFirst()
                 .orElseThrow(() -> new FileException(INVALID_FILE_EXTENSION));
     }
 
     private static String extractFileExtension(final String fileName) {
-        return fileName.substring(fileName.lastIndexOf("."));
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 }
